a9308f44a57dd6043e49f11505098914e363d48f
ls <cancel>
2021-08-30 08:07:32 -0500

    more scaffolding

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express()
 app.set('view engine', 'hbs')
 
-const port = process.env.PORT || 4000;
+const port =  4000;
 
 app.listen( port, () => { console.log(`E-compliment app running on port ${port}`)})
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 const compliments = [
     "Your instructors love you",
@@ -15,3 +17,18 @@ const compliments = [
   ]
   
   const colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+//   {newColor: randomColors, newCompliments: randomCompliments}
+
+  app.get('/', (req, res) => { 
+    // let randomCompliments = compliments[Math.floor(Math.random()*compliments.length)]
+    // let randomColors = colors[Math.floor(Math.random()*colors.length)]
+    res.render ('welcome')
+}
+)
+
+  app.get('/:name', (req, res) => { 
+      let randomCompliments = compliments
+
+      res.render('index', {name: req.params.name})
+    }
+    )
